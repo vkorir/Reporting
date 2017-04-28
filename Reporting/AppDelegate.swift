1dac6a2c7585bf8e63d5fa6bbc538f64517533ee
vkorir <vkkorir@gmail.com>
2017-04-28 16:30:44 -0700

    more updates

--- Reporting/AppDelegate.swift
+++ Reporting/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FIRApp.configure()
         GMSServices.provideAPIKey(Constants.googleMapsAPI)
         
         return true
